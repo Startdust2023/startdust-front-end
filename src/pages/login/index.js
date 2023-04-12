42bca9d2668d16dcd063a6b7d307874c37ad0ead
Luis Mukul <luis.edmch0513@gmail.com>
2023-04-12 14:48:32 -0600

    BIEN-2 corrección del login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -158,118 +158,147 @@ const LoginPage = () => {
   const imageSource = skin === 'bordered' ? 'auth-v2-login-illustration-bordered' : 'auth-v2-login-illustration'
 
   return (
-    <Box className='content-right' sx={{ backgroundColor: 'background.paper' }}>
-      {!hidden ? (
-        <Box
-          sx={{
-            flex: 1,
-            display: 'flex',
-            position: 'relative',
-            alignItems: 'center',
-            borderRadius: '20px',
-            justifyContent: 'center',
-            margin: theme => theme.spacing(8, 0, 8, 8)
-          }}
-        >
-          <LoginIllustration alt='login-illustration' src={`/images/pages/${imageSource}-${theme.palette.mode}.png`} />
-          <FooterIllustrationsV2 />
-        </Box>
-      ) : null}
-      <RightWrapper>
-        <Box
-          sx={{
-            p: 6,
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}
-        >
-          <Box sx={{ width: '100%', maxWidth: 400 }}>
-            <Box sx={{ my: 6, textAlign: 'center', color: 'primary.main' }}>
-              <Typography sx={{ mb: 1.5, fontWeight: 'bold', fontSize: '1.625rem', lineHeight: 1.385 }}>
-                {`${themeConfig.templateName}`}
-              </Typography>
-              <Typography sx={{ color: 'text.secondary' }}>Iniciar Sesión</Typography>
-              <Icon icon='mdi:user-circle' fontSize='7em' />
-            </Box>
-            <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
-              <FormControl fullWidth sx={{ mb: 4 }}>
-                <Controller
-                  name='email'
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field: { value, onChange, onBlur } }) => (
-                    <TextField
-                      autoFocus
-                      label='Correo'
-                      value={value}
-                      onBlur={onBlur}
-                      onChange={onChange}
-                      error={Boolean(errors.email)}
-                      placeholder='admin@vuexy.com'
-                    />
-                  )}
-                />
-                {errors.email && <FormHelperText sx={{ color: 'error.main' }}>{errors.email.message}</FormHelperText>}
-              </FormControl>
-              <FormControl fullWidth sx={{ mb: 1.5 }}>
-                <InputLabel htmlFor='auth-login-v2-password' error={Boolean(errors.password)}>
-                  Contraseña
-                </InputLabel>
-                <Controller
-                  name='password'
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field: { value, onChange, onBlur } }) => (
-                    <OutlinedInput
-                      value={value}
-                      onBlur={onBlur}
-                      label='Contraseña'
-                      onChange={onChange}
-                      id='auth-login-v2-password'
-                      error={Boolean(errors.password)}
-                      type={showPassword ? 'text' : 'password'}
-                      endAdornment={
-                        <InputAdornment position='end'>
-                          <IconButton
-                            edge='end'
-                            onMouseDown={e => e.preventDefault()}
-                            onClick={() => setShowPassword(!showPassword)}
-                          >
-                            <Icon icon={showPassword ? 'tabler:eye' : 'tabler:eye-off'} fontSize={20} />
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                    />
-                  )}
-                />
-                {errors.password && (
-                  <FormHelperText sx={{ color: 'error.main' }} id=''>
-                    {errors.password.message}
-                  </FormHelperText>
-                )}
-              </FormControl>
-              <Box
-                sx={{
-                  mb: 1.75,
-                  display: 'flex',
-                  flexWrap: 'wrap',
-                  alignItems: 'center',
-                  justifyContent: 'space-between'
-                }}
-              >
-                <FormControlLabel
-                  label='Recuerdame'
-                  control={<Checkbox checked={rememberMe} onChange={e => setRememberMe(e.target.checked)} />}
-                />
-                <LinkStyled href='/forgot-password'>Olvidaste tu contraseña?</LinkStyled>
-              </Box>
-            </form>
+    <>
+      <Box sx={{ p: 3, position: 'fixed', top: 0, width: '100%', zIndex: 1 }}>
+        <Card sx={{ height: 70, width: '100%', display: 'flex' }}>
+          <Box sx={{ px: 6, alignItems: 'center', width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+            <LogoSaludDigna src={'/images/salud_digna_logo.png'} />
+            &nbsp;&nbsp;
+            <LogoAplicacion src={'/images/CoraCierre.png'} />
           </Box>
-        </Box>
-      </RightWrapper>
-    </Box>
+        </Card>
+      </Box>
+      <Box className='content-right' sx={{ backgroundColor: 'background.paper' }}>
+        {!hidden ? (
+          <Box
+            sx={{
+              flex: 1,
+              display: 'flex',
+              position: 'relative',
+              alignItems: 'center',
+              borderRadius: '20px',
+              justifyContent: 'center',
+              margin: theme => theme.spacing(8, 0, 8, 8)
+            }}
+          >
+            <LoginIllustration
+              alt='login-illustration'
+              src={`/images/pages/${imageSource}-${theme.palette.mode}.png`}
+            />
+            <FooterIllustrationsV2 />
+          </Box>
+        ) : null}
+        <RightWrapper>
+          <Box
+            sx={{
+              p: 6,
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Box sx={{ width: '100%', maxWidth: 400, mt: 10 }}>
+              <Box sx={{ my: 6, textAlign: 'center', color: 'primary.main' }}>
+                <Typography sx={{ mb: 1.5, fontWeight: 'bold', fontSize: '1.625rem', lineHeight: 1.385 }}>
+                  {`${themeConfig.templateName}`}
+                </Typography>
+                <Typography sx={{ color: 'text.secondary' }}>Iniciar Sesión</Typography>
+                <Icon icon='mdi:user-circle' fontSize='7em' />
+              </Box>
+              <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+                <FormControl fullWidth sx={{ mb: 4 }}>
+                  <Controller
+                    name='email'
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field: { value, onChange, onBlur } }) => (
+                      <TextField
+                        autoFocus
+                        label='Correo'
+                        value={value}
+                        onBlur={onBlur}
+                        onChange={onChange}
+                        error={Boolean(errors.email)}
+                        placeholder='admin@vuexy.com'
+                      />
+                    )}
+                  />
+                  {errors.email && <FormHelperText sx={{ color: 'error.main' }}>{errors.email.message}</FormHelperText>}
+                </FormControl>
+                <FormControl fullWidth sx={{ mb: 1.5 }}>
+                  <InputLabel htmlFor='auth-login-v2-password' error={Boolean(errors.password)}>
+                    Contraseña
+                  </InputLabel>
+                  <Controller
+                    name='password'
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field: { value, onChange, onBlur } }) => (
+                      <OutlinedInput
+                        value={value}
+                        onBlur={onBlur}
+                        label='Contraseña'
+                        onChange={onChange}
+                        id='auth-login-v2-password'
+                        error={Boolean(errors.password)}
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                          <InputAdornment position='end'>
+                            <IconButton
+                              edge='end'
+                              onMouseDown={e => e.preventDefault()}
+                              onClick={() => setShowPassword(!showPassword)}
+                            >
+                              <Icon icon={showPassword ? 'tabler:eye' : 'tabler:eye-off'} fontSize={20} />
+                            </IconButton>
+                          </InputAdornment>
+                        }
+                      />
+                    )}
+                  />
+                  {errors.password && (
+                    <FormHelperText sx={{ color: 'error.main' }} id=''>
+                      {errors.password.message}
+                    </FormHelperText>
+                  )}
+                </FormControl>
+                <Box
+                  sx={{
+                    mb: 1.75,
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <FormControlLabel
+                    label='Recuerdame'
+                    control={<Checkbox checked={rememberMe} onChange={e => setRememberMe(e.target.checked)} />}
+                  />
+                  <LinkStyled href='/forgot-password'>Olvidaste tu contraseña?</LinkStyled>
+                </Box>
+              </form>
+              <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
+                <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 4 }}>
+                  Ingresar
+                </Button>
+                <Typography sx={{ color: 'text.secondary', mb: 4 }}>¿Nuevo en la plataforma?</Typography>
+                <Button
+                  variant='outlined'
+                  fullWidth
+                  onClick={() => {
+                    router.push(routes.register.path)
+                  }}
+                >
+                  Registrarse
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </RightWrapper>
+      </Box>
+    </>
   )
 }
 LoginPage.getLayout = page => <BlankLayout>{page}</BlankLayout>
